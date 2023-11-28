610b8cdf9ed3a9a790ff1b94d0ebc8edfe96891b
Eduardo Bach <eduardo-bach@protonmail.com>
2023-11-28 17:31:44 -0300

    DeleteTask

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -3,7 +3,7 @@ const Task = require("../models/Task");
 const getAllTasks = async (req, res) => {
     try {
         const tasksList = await Task.find();
-        return res.render("index", { tasksList, task: null });
+        return res.render("index", { tasksList, task: null, taskDelete: null });
     } catch (err) {
         res.status(500).send({ error: err.message });
     }
@@ -25,9 +25,14 @@ const createTask = async (req, res) => {
 
 const getById = async (req, res) => {
     try {
-        const task = await Task.findOne({ _id: req.params.id });
         const tasksList = await Task.find();
-        res.render("index", { task, tasksList }); // Correção aqui
+        if (req.params.method == "update") {
+            const task = await Task.findOne({ _id: req.params.id });
+            res.render("index", { task, taskDelete: null, tasksList }); // Correção aqui   
+        } else{
+            const taskDelete = await Task.findOne({ _id: req.params.id });
+            res.render("index", { task: null, taskDelete, tasksList }); // Correção aqui
+        }
     } catch (err) {
         res.status(500).send({ error: err.message });
     }
@@ -43,9 +48,19 @@ const updateOneTask = async (req, res) => {
     }
 };
 
+const deleteOneTask = async (req,res) => {
+    try {
+        await Task.deleteOne({_id: req.params.id});
+        res.redirect("/");
+    } catch (err) {
+        res.status(500).send({error: err.message});
+    }
+}
+
 module.exports = {
     getAllTasks,
     createTask,
     getById,
     updateOneTask,
+    deleteOneTask,
 };
--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,9 @@ const TaskController = require("../controller/TaskController");
 
 routes.get("/", TaskController.getAllTasks);
 routes.post("/create", TaskController.createTask);
-routes.get("/getById/:id", TaskController.getById); // Corrigindo o parâmetro da rota
+routes.get("/getById/:id/:method", TaskController.getById); // Corrigindo o parâmetro da rota
 routes.post("/updateOneTask/:id", TaskController.updateOneTask);
+routes.get("/deleteOne/:id", TaskController.deleteOneTask);
 
 module.exports = routes;
 
